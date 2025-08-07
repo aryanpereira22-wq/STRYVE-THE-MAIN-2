32fdd1b6c5b7d6ba56aa8f53085077ac40b8d089
aryanpereira22-wq <aryanpereira22@gmail.com>
2025-08-07 23:23:33 +0530

    Initial Commit

---
+++ STRYVE THE MAIN/STRYVE_THE_MAINApp.swift
@@ -0,0 +1,17 @@
+//
+//  STRYVE_THE_MAINApp.swift
+//  STRYVE THE MAIN
+//
+//  Created by Aryan Pereira on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct STRYVE_THE_MAINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
