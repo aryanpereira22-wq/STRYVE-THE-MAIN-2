b27385256d4b6b07eba349b8ebed085034e6620f
jjj <aryanpereira22@gmail.com>
2025-08-07 23:23:33 +0530

    Initial Commit

---
+++ STRYVE THE MAINTests/STRYVE_THE_MAINTests.swift
@@ -0,0 +1,17 @@
+//
+//  STRYVE_THE_MAINTests.swift
+//  STRYVE THE MAINTests
+//
+//  Created by Aryan Pereira on 07/08/2025.
+//
+
+import Testing
+@testable import STRYVE_THE_MAIN
+
+struct STRYVE_THE_MAINTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
