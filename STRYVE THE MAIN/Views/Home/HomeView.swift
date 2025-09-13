3079278fccb7026fb49c64213262790606195aba
johnpork43 <bradley17moraes@gmail.com>
2025-09-13 00:41:02 -0500

    Update HomeView.swift

--- STRYVE THE MAIN/Views/Home/HomeView.swift
+++ STRYVE THE MAIN/Views/Home/HomeView.swift
@@ -22,7 +22,7 @@ struct HomeView: View {
                     // Header
                     HStack {
                         VStack(alignment: .leading) {
-                            Text("Welcome,")
+                            Text("Welcome Back,")
                                 .font(.title3)
                                 .foregroundColor(.gray)
                             Text("Athlete")
